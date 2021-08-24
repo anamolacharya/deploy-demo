e71790659394a2ed2f125c92b96651323acc3bf9
anamolacharya <anamol222@gmailcom>
2021-08-24 12:43:13 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Amazon deploy demo</h1>
       </header>
-      <h2>This is a Amazon deploy demo</h2>
+      <h2>
+        This project is used to self learn CI/CD worlflow using github actions
+        and deploy in AWS ECR.
+      </h2>
     </div>
   );
 }
